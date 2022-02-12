f35967537a50f4aabd6d3109d9053700e065afe5
g1roy <cross657@mail.ru>
2022-02-12 23:55:35 +0300

    First commit

--- VKApp/ViewController.swift
+++ VKApp/ViewController.swift
@@ -12,7 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(#function)
     }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print(#function)
+    }
+    
+    
+    
 
 
 }
